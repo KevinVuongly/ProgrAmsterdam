8d0a22673abc099cfb2afe2c2442f8b3b29adb32
KevinVuongly <k.vuongly@gmail.com>
2018-04-17 12:48:27 +0200

    update readData

--- scan/readData.py
+++ scan/readData.py
@@ -1,16 +1,27 @@
 import csv
 
-class rushHour:
+class cars:
     def __init__(self, gridSize, puzzle):
         self.gridSize = gridSize
 
-        with open(self.puzzle) as csvfile:
+        self.changeable = []
+        self.fixed = []
+        self.richting = []
+        self.lengte = []
+        
+        with open(puzzle) as csvfile:
             readcsv = csv.reader(csvfile, delimiter = ",")
 
             for row in readcsv:
-                self.x = row[0]
-                self.y = row[1]
-                self.richting = row[2]
-                self.lengte = row[3]
+                if row[2] == 0:
+                    self.changeable.append(row[0])
+                    self.fixed.append(row[1])
+                else:
+                    self.changeable.append(row[1])
+                    self.fixed.append(row[0])
 
-        
+                self.richting.append(row[2])
+                self.lengte.append(row[3])
+
+class move:
+    
