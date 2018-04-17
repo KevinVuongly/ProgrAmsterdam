d8355d37b01c91006e819007fd6610624cf480b9
KevinVuongly <k.vuongly@gmail.com>
2018-04-17 14:43:53 +0200

    update readData

--- scan/readData.py
+++ scan/readData.py
@@ -1,6 +1,7 @@
 import csv
+import numpy as np
 
-class cars:
+class game:
     def __init__(self, gridSize, puzzle):
         self.gridSize = gridSize
 
@@ -13,15 +14,29 @@ class cars:
             readcsv = csv.reader(csvfile, delimiter = ",")
 
             for row in readcsv:
-                if row[2] == 0:
-                    self.changeable.append(row[0])
-                    self.fixed.append(row[1])
+                if row[2] == "0":
+                    self.changeable.append(int(row[0]))
+                    self.fixed.append(int(row[1]))
                 else:
-                    self.changeable.append(row[1])
-                    self.fixed.append(row[0])
+                    self.changeable.append(int(row[1]))
+                    self.fixed.append(int(row[0]))
 
-                self.richting.append(row[2])
-                self.lengte.append(row[3])
+                self.richting.append(int(row[2]))
+                self.lengte.append(int(row[3]))
+        
+    def visualize(self):
+        grid = [["-" for x in range(self.gridSize)] for y in range(self.gridSize)]
+  
+        for i in range(len(self.changeable)):
+            if self.richting[i] == 1:
+                for j in range(self.lengte[i]):
+                    grid[self.changeable[i] + j][self.fixed[i]] = chr(i + 97)
+            else:
+                for j in range(self.lengte[i]):
+                    grid[self.fixed[i]][self.changeable[i] + j] = chr(i + 97)
 
-class move:
-    
+        for el in grid:
+            print(" ".join(map(str, el)))
+
+test = game(6, "../data/game1.csv")
+test.visualize()
