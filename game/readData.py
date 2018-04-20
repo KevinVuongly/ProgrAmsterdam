d813c1e82d28e1991db7f6cf13dcea6d5379ea60
KevinVuongly <k.vuongly@gmail.com>
2018-04-20 10:44:22 +0200

    update repo

--- game/readData.py
+++ game/readData.py
@@ -14,29 +14,34 @@ class game:
             readcsv = csv.reader(csvfile, delimiter = ",")
 
             for row in readcsv:
-                if row[2] == "0":
+                if row[2] == "h":
                     self.changeable.append(int(row[0]))
                     self.fixed.append(int(row[1]))
                 else:
                     self.changeable.append(int(row[1]))
                     self.fixed.append(int(row[0]))
 
-                self.richting.append(int(row[2]))
+                self.richting.append(row[2])
                 self.lengte.append(int(row[3]))
         
     def visualize(self):
         grid = [["-" for x in range(self.gridSize)] for y in range(self.gridSize)]
-  
+
+
+        for j in range(self.lengte[0]):
+            grid[self.fixed[0]][self.changeable[0] + j] = "*"
+            
         for i in range(len(self.changeable)):
-            if self.richting[i] == 1:
-                for j in range(self.lengte[i]):
-                    grid[self.changeable[i] + j][self.fixed[i]] = chr(i + 97)
-            else:
-                for j in range(self.lengte[i]):
-                    grid[self.fixed[i]][self.changeable[i] + j] = chr(i + 97)
+            if i > 0:
+                if self.richting[i] == "v":
+                    for j in range(self.lengte[i]):
+                        grid[self.changeable[i] + j][self.fixed[i]] = chr(i + 97)
+                else:
+                    for j in range(self.lengte[i]):
+                        grid[self.fixed[i]][self.changeable[i] + j] = chr(i + 97)
 
         for el in grid:
             print(" ".join(map(str, el)))
 
-test = game(6, "../data/game1.csv")
+test = game(6, "../data/game3.csv")
 test.visualize()
