1f9c85f99d62ace8d72f775bd13da0d8e943a0b7
Michael Berend <michaelberend@hotmail.com>
2018-04-26 14:13:30 +0200

    Added information for groupmembers

--- classes/Node.py
+++ classes/Node.py
@@ -12,3 +12,9 @@ def nextMove
 
 
 def children
+	?
+
+"""
+    K&J -> Kindjes aan maken via ook de possible moves van de board (elke board class is een state)
+    Dan heb je nodes die je kunt traversen en begint het ergens op te lijken
+"""
