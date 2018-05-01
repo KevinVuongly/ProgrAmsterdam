e99bfff0613bc2b0111743d33563ff17109b5a4c
Michael Berend <michaelberend@hotmail.com>
2018-05-01 14:03:20 +0200

    changes in main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from Board import Board
+from ReadBoard import ReadBoard
+import sys
 
 def main():
+	bfs()
 	
 
-if __name__ = "__main__"
+if __name__ == "__main__"
 	main()
