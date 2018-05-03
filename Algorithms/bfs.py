515fab8cffe46724f56630db0bb88fede291f97a
Michael Berend <michaelberend@hotmail.com>
2018-05-03 14:41:43 +0200

    Minor change in semiRandomSelection code

--- Algorithms/bfs.py
+++ Algorithms/bfs.py
@@ -10,6 +10,7 @@ class Bfs:
 
 
     def randomSelection(self):
+        """ Looks for all moves possible and randomly choses one until solved """
         counter = 0
         child = []
         while self.board.checkSolution != 0:
@@ -32,6 +33,42 @@ class Bfs:
                 continue
             else:
                 self.board.changeable = random.choice(children)
+        return self.board
+
+        def semiRandomSelection(self):
+        """ Looks for all moves possible and randomly choses one until solved, without
+        back to board configurations visited before. This code can crash if the only way 
+        back is an older configuration """
+        counter = 0
+        child = []
+        visited = [] 
+        while self.board.checkSolution != 0:
+            counter += 1
+            children = []
+            j = 0
+            posMoves = self.board.checkPossibleMoves()
+            print(counter)
+            for i in range(self.board.nrOfCars):
+                for move in posMoves[i]:
+                    if not move:
+                        continue
+                    else:
+                        child = copy.deepcopy(self.board.changeable)
+                        child[i] = child[i] + posMoves[i][j]
+                        children.append(child)
+                    j += 1
+                j = 0
+            if not children:
+                continue
+            else:
+                self.board.changeable = random.choice(children)
+                j = 0
+                while self.board.changeable in visited
+                    self.board.changeable = random.choice(children)
+                    j += 1
+                    if j > 20
+                        return ValueError('The code has led itself into a corner')
+                visited.append = self.board.changeable
         return self.board
 
 
