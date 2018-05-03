4dfb3d70f93aa5b518f80e150bafc0f23c799a77
Michael Berend <michaelberend@hotmail.com>
2018-05-03 12:29:57 +0200

    Made changes in bfs

--- Algorithms/bfs.py
+++ Algorithms/bfs.py
@@ -1,8 +1,22 @@
-
+from classes.Board import Board
+import copy
 
 
 class Bfs:
-  
+    def __init__(self, board):
+        """Initialisation of the board's parameters.
+
+        Args:
+            gridSize (int): An integer for the size of the grid.
+            changeable (list of ints): An int for the variable position of car i.
+            fixed (list of ints): An int for the fixed position of car i.
+            direction (list of strings): A character for the direction the car i is facing.
+            length (list of ints): The length of car i.
+            nrOfCars (int): The number of cars on the grid.
+        """
+        self.board = board
+
+
 
 
     def breadFS(self):
@@ -21,4 +35,15 @@ class Bfs:
                     node = 
 
 
+
+
+        while stack:
+            cur_node = stack[0]
+            stack = stack[1:]
+            nodes.append(cur_node)
+            moves = checkPossibleMoves()
+            children 
+            for child in cur_node.get_children():
+                stack.append(child)
+        return nodes
 
