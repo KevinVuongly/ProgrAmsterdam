d7286b82fc8a3daadc317715aa3da1076cc9f479
Michael Berend <michaelberend@hotmail.com>
2018-05-04 14:08:42 +0200

    Deleted unneccesary commenting

--- algorithms/random.py
+++ algorithms/random.py
@@ -88,38 +88,3 @@ class Random:
         print("WE HAVE FOUND IT!")
         print(visited)
         return self.board
-    """
-    def breadFS(self):
-        nodes = [] self.board.changeable
-        beginState = self.changeable
-        posMoves = checkPossibleMoves(beginState)
-        for i in range(self.board.nrOfCars)
-            for j rang(len(posMoves[j]))
-                child = self.board.changeable
-                child[i] = child[i] + posMoves[i][j]
-
-        select
-
-        #Create the first set of children
-        for i in range(self.nrOfCars)
-            for j rang(len(posMoves[j]))
-                child = self.changeable
-                child[i] = child[i] + posMoves[i][j]
-                explored = []
-                queue = [start]
-                while true:
-                    node =
-
-
-
-
-        while stack:
-            cur_node = stack[0]
-            stack = stack[1:]
-            nodes.append(cur_node)
-            moves = checkPossibleMoves()
-            children
-            for child in cur_node.get_children():
-                stack.append(child)
-        return nodes
-    """
