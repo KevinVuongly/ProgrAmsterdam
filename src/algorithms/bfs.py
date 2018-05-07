172c3adfd60221bdcfd5f2c8e7d4a2e8daf8081d
KevinVuongly <k.vuongly@gmail.com>
2018-05-07 12:46:18 +0200

    repository changes

---
+++ src/algorithms/bfs.py
@@ -0,0 +1,78 @@
+from classes.Board import Board
+from copy import copy, deepcopy
+
+class BFS:
+        def __init__(self, game, board, colors):
+            """
+            Takes all information of the board with it's state as the beginning of the game.
+            """
+            self.game = game
+            self.board = board
+            self.colors = colors
+
+        def bfs(self):
+            """
+            Runs breadth first search on the initialized board. The algorithm stops when
+            it finds a solution e.g. a state which the red car can move to the end.
+            """
+
+            self.queue = [self.board.changeable]
+
+            self.archive = {}
+            self.archive[str(self.board.changeable)] = "beginning!"
+
+            while self.board.checkSolution() != 0 and len(self.archive) < 1000000:
+
+                children = self.board.createChildren()
+
+                amountOfChildren = len(children)
+                childToCheck = 0
+
+                while childToCheck != amountOfChildren:
+                    children, childToCheck, amountOfChildren = self.removeRedundantChild(children, childToCheck, amountOfChildren)
+
+                if len(children) > 0:
+                    self.addToArchive(children)
+
+                self.queue.pop(0)
+                self.board.changeable = self.queue[0]
+
+            self.pathSolution(self.board.changeable)
+
+        def removeRedundantChild(self, children, childPos, childrenLeft):
+            """
+            Removes duplicate state e.g. child that is already listed in the archive
+            """
+            if str(children[childPos]) in self.archive.keys():
+                children.pop(childPos)
+                childrenLeft -= 1
+            else:
+                childPos += 1
+
+            return children, childPos, childrenLeft
+
+        def addToArchive(self, childrenOfState):
+            """
+            Add's child as key, with the parent as the value to the archive dictionary.
+            """
+            for i in range(len(childrenOfState)):
+                self.queue.append(childrenOfState[i])
+                self.archive[str(childrenOfState[i])] = self.board.changeable
+
+        def pathSolution(self, solutionState):
+            """
+            Visualizes the path found through breadth first search.
+            """
+            path = [solutionState]
+
+            child = solutionState
+            parent = self.archive[str(solutionState)]
+
+            while self.archive[str(child)] != "beginning!":
+                path.insert(0, parent)
+
+                child = parent
+                parent = self.archive[str(child)]
+
+            for i in range(len(path)):
+                self.board.visualize(path[i], self.colors, "BFS", self.game, i)
---
+++ src/main.py
@@ -0,0 +1,55 @@
+import time
+
+from classes.Board import Board
+from classes.ReadBoard import ReadBoard
+from algorithms.random import Random
+from algorithms.bfs import BFS
+
+def main():
+
+	while True:
+
+		firstgame = 1
+		lastgame = 7
+
+		data = input("Pick the game you want to solve for (1, 2, 3,... , {}): ".format(lastgame))
+
+		try:
+			data = int(data)
+		except:
+			print("Please pick an existing game.")
+
+		if isinstance(data, int):
+
+			if data < firstgame or data > lastgame:
+				print("Please pick an existing game.")
+			else:
+				break
+
+	while True:
+		algorithm = input("Pick which algorithm you want to use(random, BFS): ")
+		if algorithm != "random" and algorithm != "BFS":
+			print("Please pick a correct algorithm.")
+		else:
+			file = "data/game" + str(data) + ".csv"
+
+			beginState = ReadBoard(file)
+
+			game = Board(beginState.gridSize, beginState.changeable, beginState.fixed, beginState.direction, beginState.length)
+
+			print ("[" + (time.strftime("%H:%M:%S")) + "]" + " Running algorithm...")
+			break
+
+	if algorithm == "random":
+		loadGame = Random(game)
+
+		solvedgame = loadGame.randomSelection()
+		solvedgame.visualize(solvedgame.changeable, beginState.colors)
+	elif algorithm == "BFS":
+		loadGame = BFS(data, game, beginState.colors)
+		solvedgame = loadGame.bfs()
+
+	print ("[" + (time.strftime("%H:%M:%S")) + "]" + " Solution found.")
+
+if __name__ == "__main__":
+	main()
